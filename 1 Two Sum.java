513ea1b40b36a3734335321d2f969e3e2083ac95
rock4you <rockpeng@foxmail.com>
2017-10-19 12:51:49 +0300

    https://leetcode.com/problems/two-sum/description/
    Given an array of integers, return indices of the two numbers such that they add up to a specific target.
    
    You may assume that each input would have exactly one solution, and you may not use the same element twice.
    
    Example:
    Given nums = [2, 7, 11, 15], target = 9,
    
    Because nums[0] + nums[1] = 2 + 7 = 9,
    return [0, 1].

---
+++ 1 Two Sum.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+		HashMap <Integer,Integer> amap = new HashMap();
+		int[] index = new int [2];
+		for(int i=0;i<nums.length;i++)
+		{
+			amap.put(nums[i], i);
+		}
+		for(int i=0;i<nums.length;i++)
+		{
+			if(amap.containsKey(target-nums[i]) && nums[i]!=(target/2))
+			{
+				index[0]=i;
+				index[1]=amap.get(target-nums[i]);
+				break;
+			}
+		}
+		return index;
+    }
+}
